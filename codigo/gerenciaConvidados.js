6e03483e2238918d130fba4b92b390cbc451839d
Luis Brescia <luisfelipebrescia@gmail.com>
2023-04-24 22:14:02 -0300

    adicionado interfaces

--- codigo/gerenciaConvidados.js
+++ codigo/gerenciaConvidados.js
@@ -1,11 +1,21 @@
-const divConvidados = document.getElementById("exibeJanela")
+const telaInterface = document.getElementById("exibeInterface")
+const nomeInterface = document.getElementById("interfaceNome")
 
 // Função que abre a página de convidados
-function exibeConvidados(){
-    if(divConvidados.style.display === "block"){
-       divConvidados.style.display = "none";
-      }
-      else{
-         divConvidados.style.display = "block";
-    }
+function exibeInterface(botao){
+    telaInterface.style.display = "block";
+    nomeInterface.textContent = botao.innerHTML;
+}
+function fechaInterface(){
+    telaInterface.style.display = "none";
+}
+
+function confirma(botao){
+    let action = botao.innerHTML;
+    action = action.toLowerCase();
+    // Remover os 2 últimos caracteres da string
+    action = action.substring(0, action.length - 2);
+    action = action + "ido";
+    action = action.charAt(0).toUpperCase() + action.slice(1);
+    window.alert(action + " com sucesso.")
 }
