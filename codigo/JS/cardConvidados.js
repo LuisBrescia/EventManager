9c896a082d51717488381c6cc8c5f288b95b8b21
Luis Brescia <luisfelipebrescia@gmail.com>
2023-05-18 17:51:51 -0300

    Linhas em branco são excluídas automaticamente, solução provisória para problema de movimentar cards

--- codigo/JS/cardConvidados.js
+++ codigo/JS/cardConvidados.js
@@ -5,6 +5,7 @@ var maxWidth = 250; // Máximo de pixels permitidos no título
 var maxLength = 15; // Máximo de caracteres permitidos no título
 
 var contador = 1; // Contador de cards
+var ordem = 1;    // Ordem em que os cards foram adicionados
 
 // > Preciso que cards com position diferentes tenham a mesma largura
 // > Preciso que todos os cards manteham suas coordenadas após mudança de posição
@@ -12,8 +13,8 @@ var contador = 1; // Contador de cards
 // ? Botão de adicionar card
 adicionaCard.on('click', () => {
 
-    // * Limite de 4 cards
-    if (canvas.children().length >= 5) {
+    // * Limite de 6 cards
+    if (canvas.children().length >= 6) {
         const toastBootstrap = bootstrap.Toast.getOrCreateInstance($('#liveToast'))
         toastBootstrap.show();
         setTimeout(() => { toastBootstrap.hide(); }, 3000);
@@ -24,18 +25,15 @@ adicionaCard.on('click', () => {
         // Se colocado position absolute aqui, outros cards não serão movidos para baixo
         // Porém acontecerá bugs de cards se tornarem imovéis
         // > Adicionar e remover propriedade position absolute quando necessário
-
         // ? Possível solução:          (Farei quando tiver tempo)
-        /* 
-        > Cards que não estão sendo utilizados, terão position fixed       
-        */
-
+        // > Cards que não estão sendo utilizados, terão position fixed       
         // ? Acredito que a solução envolva mudar algo desta linha de baixo
-        '<div class="col-10 card-container ms-5" style="top:'+ contador +'0%">' +
-        '<div class="cardConvidado draggable card col-3 shadow border-0 rounded-3 overflow-x-hidden" style="max-height:300px">' +
+        '<div id="card-' + ordem + '" class="col-10 card-container ms-5" style="top:' + contador + '0%">' +
+        '<div class="cardConvidado draggable card col-3 shadow border-0 rounded-3 overflow-x-hidden"' +
+        'style="max-height:300px; width:300px">' +
         '<div class="card-header fs-4 fw-bolder text-nowrap d-flex justify-content-between align-items-center">' +
-        '<span class="mt-3 lh-1 pt-2">' +
-        'Lista' + contador + 
+        '<span class="mt-3 mb-1 lh-1 pt-2">' +
+        'Lista' + contador + '_id:' + ordem +
         '</span>' +
         '<span class="position-absolute end-0 top-0 m-0 p-0">' +
         '<button type="button" class="btn bi-arrow-clockwise resetaCard btn-sm"></button>' +
@@ -58,17 +56,28 @@ adicionaCard.on('click', () => {
         snap: false,
         stack: ".draggable",
         cursor: "grabbing",
-        handle: ".card-header",
+        handle: ".card-header"//,
+        // stop: function () {
+        //     // > Se a ID do card
+        //     if (contador <= 2) {
+        //         // * Só funciona na primeira vez porque o calor não é resetado
+        //         if ($('.funcionalidades:hover').length > 0) {
+        //             $(this).css('top', '10px');
+        //         } 
+        //         $(this).closest('.card-container').css('position', 'static');
+        //     }
+        // }
     });
+
     // * Editar o conteúdo do card
     newCard.find('.list-group').on('keydown', function (e) {
-        
         // ? Apenas para simplificar o código
         var listGroup = $(this).closest('.card-container').find('.list-group');
+        var otherCards = $('.card-container').not($(this).closest('.card-container'));
         var currentLi = listGroup.find('li:focus');
         var currentLiText = listGroup.find('li:focus').text();
 
-        // Se o usuário pressionar backspace e a linha estiver vazia, e não houver apenas 1 linha, linha atual é apagada, e o focus irá para a linha anterior
+        // * Se o usuário pressionar backspace e a linha estiver vazia, e não houver apenas 1 linha, linha atual é apagada, e o focus irá para a linha anterior
         if ((e.keyCode === 8 || e.keyCode === 38 || e.keyCode === 40) && currentLiText === '' && listGroup.children().length > 1) {
             e.preventDefault();
             currentLi.remove();
@@ -87,6 +96,7 @@ adicionaCard.on('click', () => {
             // > Por algum motivo, todos os cards são afetados pelas mudanças
             // > Talvez seja necessário criar uma classe específica para cada card
             var liVazia = $('<li class="list-group-item fw-lighter" contenteditable="true"></li>').text("");
+            // otherCards.css('position', 'fixed');
             $(this).closest('.card-container').find('.list-group').append(liVazia);
             liVazia.focus();
         }
@@ -102,9 +112,24 @@ adicionaCard.on('click', () => {
             currentLi.next().focus();
             finalDaLinha(currentLi.next()[0]);
         }
-        $(document).on('click', function(e) {
-            if (!$(e.target).closest('.card-container').length) {
-                $('.card-container').css('position', 'fixed'); // ! Desenvolvimento
+        // * Remove li vazias quando um click é realizado
+        $(document).on('click', function() {
+            // > Aqui possívelmente está gastando mais processamento que o necessário
+            $('.card-container').each(function() {
+                removerLiVazias($(this));
+            });
+        });
+        
+        $(document).on('click', function (e) {
+            var currentLi = $('.list-group li:focus');
+            var currentLiText = currentLi.text();
+            var listGroup = currentLi.closest('.list-group');
+
+            if ((!$(e.target).closest('.card-container').length) && currentLiText === '' && listGroup.children().length > 1) {
+                e.preventDefault();
+                currentLi.remove();
+                var lastLi = listGroup.find('li:last-child').focus();
+                finalDaLinha(lastLi[0]);
             }
         });
     });
@@ -148,16 +173,16 @@ adicionaCard.on('click', () => {
                 var currentLength = $(this).text().length;
 
                 var isContentSelected = isTextSelected(cardTitle[0]);
-                
-                if ((currentWidth >= maxWidth || currentLength >= maxLength ) && !isContentSelected && e.keyCode !== 8 && e.keyCode !== 46 && !e.ctrlKey) {
+
+                if ((currentWidth >= maxWidth || currentLength >= maxLength) && !isContentSelected && e.keyCode !== 8 && e.keyCode !== 46 && !e.ctrlKey) {
                     e.preventDefault();
-                } 
+                }
             }
             if (currentLength > maxLength) {
                 var trimmedText = $(this).text().substring(0, maxLength);
                 cardTitle.text(trimmedText);
-              }
-              
+            }
+
         });
         // * Permite alterar o título caso conteúdo esteja inteiramente selecionado
         function isTextSelected(element) {
@@ -180,16 +205,19 @@ adicionaCard.on('click', () => {
         $(this).closest('.card-container').remove();
         contador--;
     });
-
     contador++;
+    ordem++;
+    $('.info').addClass('d-none');
     canvas.append(newCard); // Adiciona o novo card ao canvas
 });
 // * Apaga todos os cards
+// * ID só é reiniciado quando todos cards são apagados 
 $('#removeCard').on('click', () => {
     $('.card').parent().remove();
     contador = 1;
+    ordem = 1;
+    $('.info').removeClass('d-none');   
 });
-
 // * Cursor no final de cada linha
 function finalDaLinha(element) {
     var range = document.createRange();
@@ -198,4 +226,13 @@ function finalDaLinha(element) {
     range.collapse(true);
     sel.removeAllRanges();
     sel.addRange(range);
+}
+// * Remove li vazias
+function removerLiVazias(cardContainer) {
+    cardContainer.find('.list-group-item').each(function() {
+        // * Vai remover todos os espaços em branco, e checar se ainda resta alguma coisa
+        if ($(this).text().trim() === '' && cardContainer.find('.list-group-item').length > 1) {
+            $(this).remove();
+        }
+    });
 }
