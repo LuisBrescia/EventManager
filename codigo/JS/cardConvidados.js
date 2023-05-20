29885e33893a25ef1ee79877ff15e12e175c028b
Luis Brescia <luisfelipebrescia@gmail.com>
2023-05-20 17:41:04 -0300

    Títulos de cards já estão sendo salvos no localStorage

--- codigo/JS/cardConvidados.js
+++ codigo/JS/cardConvidados.js
@@ -8,7 +8,26 @@ var todasListas = [];
 
 // > Preciso que cards com position diferentes tenham a mesma largura
 // > Preciso que todos os cards manteham suas coordenadas após mudança de posição
-
+// * Carrega os cards salvos no localStorage
+$(document).ready(function () {
+    for (var i = 0; i < TAM; i++) {
+        var lista = {
+            Lista: i,
+            titulo: localStorage.getItem('titulo-' + i),
+            linhas: ["elemento1", "elemento2", "elemento3"],
+            quantidade: 10
+        };
+        todasListas.push(lista);
+        var carregaCard = $(criaConteudo(i, localStorage.getItem('titulo-' + i)));
+        customDrag(carregaCard);
+        editaCard(carregaCard);
+        nomeiaCard(carregaCard);
+        resetaCard(carregaCard);
+        removeCard(carregaCard);
+        vetor[i] = false;
+        $('section').append(carregaCard);
+    }
+});
 // ? Botão de adicionar card
 $('#adicionaCard').on('click', () => {
     // * Percorre o vetor, se existir algum elemento com o valor true, criará card
@@ -18,37 +37,87 @@ $('#adicionaCard').on('click', () => {
         return;
     }
     // : Sempre que um card é criado, é criado também um objeto que vai guardar as informações daquele card
-    // * Conteúdo HTML do card
-    // ! Periódicamente cards se tornam impossíveis de serem arrastados	
-    // ! Realizar testes para checar se foi corrigido
-    // > Adicionar e remover propriedade position absolute quando necessário
-    // ? Possível solução:          (Farei quando tiver tempo)
-    // > Cards que não estão sendo utilizados, terão position fixed       
-    // ? Acredito que a solução envolva mudar algo desta linha de baixo
+    // * Realizar testes para checar se foi corrigido
+    // ! Talvez cards se tornem impossíveis de serem arrastados	
     var newCard = $(criaConteudo(vetor.indexOf(true), 'Lista ' + (vetor.indexOf(true) + 1)));
-    // * Função para configurar regras para movimentação do card
-    newCard.find(".draggable").draggable({
+    // ? É como se estivessemos aidicionando funcionalidades que um card é capaz de fazer
+    customDrag(newCard);
+    editaCard(newCard);
+    nomeiaCard(newCard);
+    resetaCard(newCard);
+    removeCard(newCard);
+    localStorage.setItem('titulo-' + vetor.indexOf(true), 'Lista ' + (vetor.indexOf(true) + 1));
+    vetor[vetor.indexOf(true)] = false;
+    $('section').append(newCard);
+    
+    $('.info').addClass('d-none');
+});
+// * Apaga todos os cards
+$('#removeCard').on('click', () => {
+    $('.card').parent().remove();
+    vetor.fill(true);
+    $('.info').removeClass('d-none');
+});
+// * No momento #salvaCard está sendo utilizado para esconder o menu lateral
+$('#salvaCard').on('click', () => {
+    $('.card').find('.card-container').removeClass('mover');
+});
+// * Cursor no final de cada linha
+function finalDaLinha(element) {
+    if (element && element.childNodes && element.childNodes.length > 0) {
+        var range = document.createRange();
+        var sel = window.getSelection();
+        range.setStart(element.childNodes[0], element.childNodes[0].length);
+        range.collapse(true);
+        sel.removeAllRanges();
+        sel.addRange(range);
+    }
+}
+// * Remove li vazias
+function removerLiVazias(cardContainer) {
+    cardContainer.find('.list-group-item').each(function () {
+        // * Vai remover todos os espaços em branco, e checar se ainda resta alguma coisa
+        if ($(this).text().trim() === '' && cardContainer.find('.list-group-item').length > 1) {
+            $(this).remove();
+        }
+    });
+}
+// * Atualiza o título do card
+function atualizaTitulo(element) {
+    var novoNome = element.text().trim();
+    if (novoNome !== '') {
+        element.text(novoNome);
+        var cardId = element.closest('.card-container').attr('id');
+        var cardNumber = parseInt(cardId.split('-')[1]);
+        localStorage.setItem('titulo-' + cardNumber, novoNome);
+        console.log('Título salvo no card de ID ' + cardNumber + ' como: ' + novoNome);
+    }
+}
+// * Permite alterar o título caso conteúdo esteja inteiramente selecionado
+function isTextSelected(element) {
+    var selection = window.getSelection();
+    var selectedText = selection.toString();
+    var elementText = element.textContent;
+    return selectedText === elementText;
+}
+// * Função para configurar regras para movimentação do card
+function customDrag(elemento) {
+    elemento.find(".draggable").draggable({
         containment: "section",
         scroll: false,
         snap: false,
         stack: ".draggable",
         cursor: "grabbing",
         handle: ".card-header",
-        // * Funão chamada quando o card é solto
         stop: function () {
             $(this).closest('.card-container').css('position', 'absolute');
             // > Descobrir como a coordenada é calculada
             // * Adicionando 300 estará me falando onde o card acaba
             var cardPosition = $(this).position().left + 48 + 300;
             var canvasWidth = $('section').width();
-            // ? ASIDE IS ACTIVE
-            // : IF TRUE NÃO FAZER NADA
             if (!$('aside').hasClass('active')) {
-                // : PEGAR A LARGURA DO CANVAS SUBTRAIR POR 250
                 larguraPermitida = canvasWidth - 250;
-                // : SE ALGUM CARD ESTIVER COM A COORDENADA MAIOR QUE A LARGURA PERMITIDA
                 if (cardPosition > larguraPermitida && !$(this).hasClass('mover')) {
-                    // : VAI SER ADICIONADA A ELE A CLASSE MOVER
                     $(this).addClass('mover');
                     console.log('Mover adicionado');
                     $(this).find('.card-header').css('background-color', '#f0f0f0');
@@ -62,12 +131,13 @@ $('#adicionaCard').on('click', () => {
             // : MOVER = COORDENADA DO CARD - LARGURA PERMITIDA
             // : E QUANDO O USUÁRIO APERTAR O BOTÃO DE MENU, VOU MOVER O CARD
             // : TRANSFORM: TRANSLATEX(-MOVER)
-            // console.log('Coordenada X do card:', cardPosition);
+            // : console.log('Coordenada X do card:', cardPosition);
         }
     });
-
-    // * Editar o conteúdo do card quando clicar em alguma linha
-    newCard.find('.list-group').on('keydown', function (e) {
+}
+// * Editar o conteúdo do card quando clicar em alguma linha
+function editaCard(element) {
+    element.find('.list-group').on('keydown', function (e) {
         // ? Apenas para simplificar o código
         var listGroup = $(this).closest('.card-container').find('.list-group');
         var otherCards = $('.card-container').not($(this).closest('.card-container'));
@@ -129,8 +199,10 @@ $('#adicionaCard').on('click', () => {
         //     }
         // });
     });
-    // * Botão que edita o título do card
-    newCard.find('.nomeiaCard').on('click', function () {
+}
+// * Botão que edita o título do card
+function nomeiaCard(element) {
+    element.find('.nomeiaCard').on('click', function () {
         var cardContainer = $(this).closest('.card-container');
         var cardTitle = cardContainer.find('.card-header span:first-child');
         cardTitle.attr('contenteditable', 'true');
@@ -175,117 +247,27 @@ $('#adicionaCard').on('click', () => {
             }
         });
     });
-    // * Botão que reseta o conteúdo do card
-    newCard.find('.resetaCard').on('click', function () {
+}
+// * Botão que reseta o conteúdo do card
+function resetaCard(element) {
+    element.find('.resetaCard').on('click', function () {
         $(this).closest('.card-container').find('.list-group li').remove();
         var liVazia = $('<li class="list-group-item fw-lighter" contenteditable="true"></li>').text("");
         $(this).closest('.card-container').find('.list-group').append(liVazia);
         // > Adiciona Position Absolute ao card
     });
-    // * Botão que apaga o card
-    newCard.find('.removeCard').on('click', function () {
+}
+// * Botão que apaga o card
+function removeCard(element) {
+    element.find('.removeCard').on('click', function () {
         var cardId = $(this).closest('.card-container').attr('id');
         var cardNumber = parseInt(cardId.split('-')[1]);
         vetor[cardNumber] = true;
         $(this).closest('.card-container').remove();
     });
-    localStorage.setItem('titulo-' + vetor.indexOf(true), 'Lista ' + (vetor.indexOf(true) + 1));
-    $('.info').addClass('d-none');
-    vetor[vetor.indexOf(true)] = false;
-    $('section').append(newCard);
-});
-
-// * Apaga todos os cards
-$('#removeCard').on('click', () => {
-    $('.card').parent().remove();
-    vetor.fill(true);
-    $('.info').removeClass('d-none');
-});
-// * No momento #salvaCard está sendo utilizado para esconder o menu lateral
-$('#salvaCard').on('click', () => {
-    $('.card').find('.card-container').removeClass('mover');
-});
-// * Cursor no final de cada linha
-function finalDaLinha(element) {
-    if (element && element.childNodes && element.childNodes.length > 0) {
-        var range = document.createRange();
-        var sel = window.getSelection();
-        range.setStart(element.childNodes[0], element.childNodes[0].length);
-        range.collapse(true);
-        sel.removeAllRanges();
-        sel.addRange(range);
-    }
 }
-// * Remove li vazias
-function removerLiVazias(cardContainer) {
-    cardContainer.find('.list-group-item').each(function () {
-        // * Vai remover todos os espaços em branco, e checar se ainda resta alguma coisa
-        if ($(this).text().trim() === '' && cardContainer.find('.list-group-item').length > 1) {
-            $(this).remove();
-        }
-    });
-}
-// * Atualiza o título do card
-function atualizaTitulo(element) {
-    var novoNome = element.text().trim();
-    if (novoNome !== '') {
-        element.text(novoNome);
-        var cardId = element.closest('.card-container').attr('id');
-        var cardNumber = parseInt(cardId.split('-')[1]);
-        localStorage.setItem('titulo-' + cardNumber, novoNome);
-        console.log('Título salvo no card de ID ' + cardNumber + ' como: ' + novoNome);
-    }
-}
-// * Permite alterar o título caso conteúdo esteja inteiramente selecionado
-function isTextSelected(element) {
-    var selection = window.getSelection();
-    var selectedText = selection.toString();
-    var elementText = element.textContent;
-    return selectedText === elementText;
-}
-
-for (var i = 0; i < TAM; i++) {
-    var lista = {
-        Lista : i,
-        titulo: localStorage.getItem('titulo-' + i),
-        linhas : ["elemento1", "elemento2", "elemento3"],
-        quantidade : 10
-    };
-    todasListas.push(lista);
-    var carregaCard = $(criaConteudo(i, localStorage.getItem('titulo-' + i)));
-    vetor[i] = false;
-    $('section').append(carregaCard);
-}
-// localStorage.setItem("Listas", JSON.stringify(todasListas));
-
-// Com esse código já é possível salvar o conteúdo de cada card
-// Após a página ser carregada
-// $(document).ready(function () {
-//     todasListas = JSON.parse(localStorage.getItem("Listas"));
-//     console.log("TODAS LISTAS",todasListas);
-//     console.log(todasListas[i].titulo);
-//     console.log(todasListas.length);
-//     for (var i = 0; i < todasListas.length; i++) {
-//         carregaCard = $(criaConteudo(todasListas[i].Lista, todasListas[i].titulo));    
-//         vetor[todasListas[i].Lista] = false;
-//         $('section').append(carregaCard);
-//     }
-// });
-
-// function salvar() {
-//     var lista = {
-//         Lista : 1,
-//         titulo: "Titulo",
-//         linhas : ["elemento1", "elemento2", "elemento3"],
-//         quantidade : 10
-//     };
-//     todasListas.push(lista);
-//     localStorage.setItem("Listas", JSON.stringify(todasListas));
-// }
-
-// No caso de cima, elemento seria vetor.indexOf(true)
+// * HTML de um card
 function criaConteudo(elemento, titulo) {
-        
     return '<div id="card-' + elemento + '" class="card-container ms-5" style="top:' +
         (elemento + 1) + '0%">' +
         '<div class="cardConvidado draggable card col-3 shadow border-0 rounded-3 overflow-x-hidden"' +
@@ -308,3 +290,28 @@ function criaConteudo(elemento, titulo) {
         '</div>' +
         '</div>';
 }
+
+/* localStorage.setItem("Listas", JSON.stringify(todasListas));
+ Com esse código já é possível salvar o conteúdo de cada card
+ Após a página ser carregada
+ $(document).ready(function () {
+     todasListas = JSON.parse(localStorage.getItem("Listas"));
+     console.log("TODAS LISTAS",todasListas);
+     console.log(todasListas[i].titulo);
+     console.log(todasListas.length);
+     for (var i = 0; i < todasListas.length; i++) {
+         carregaCard = $(criaConteudo(todasListas[i].Lista, todasListas[i].titulo));    
+         vetor[todasListas[i].Lista] = false;
+         $('section').append(carregaCard);
+     }
+ });
+ function salvar() {
+     var lista = {
+         Lista : 1,
+         titulo: "Titulo",
+         linhas : ["elemento1", "elemento2", "elemento3"],
+         quantidade : 10
+     };
+     todasListas.push(lista);
+     localStorage.setItem("Listas", JSON.stringify(todasListas));
+ }*/
