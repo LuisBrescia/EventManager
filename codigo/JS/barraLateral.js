6aeb6e92234c192f1ddcbec81153ceab33e9c3b7
Luis Brescia <luisfelipebrescia@gmail.com>
2023-05-25 00:50:02 -0300

    Começo da decoração

--- codigo/JS/barraLateral.js
+++ codigo/JS/barraLateral.js
@@ -51,17 +51,3 @@ $(document).on('keyup', function (e) {
         allowCtrl = true;
     }
 });
-
-const accordionItems = document.querySelectorAll('.accordion-collapse')
-const acc = document.getElementById('accordionExample')
-
-accordionItems.forEach((el)=>{
-    el.addEventListener('shown.bs.collapse',(e)=>{
-        var scrollOffset = acc.scrollTop + el.parentNode.offsetTop
-        acc.scroll({
-            top: scrollOffset,
-            left: 0, 
-            behavior: 'smooth'
-        })
-    })
-})
--- codigo/JS/collection.js
+++ codigo/JS/collection.js
@@ -1,53 +1,43 @@
-function executeOnScreenResize() {
-    if (window.matchMedia("(min-width: 768px)").matches) {
-        $('.colParticipantes').removeClass('d-none');
-        $('.colInsumos').removeClass('d-none');
-        $('.colServicos').removeClass('d-none');
-    } else {
-        $('#collectionParticipantes').on('click', function () {
-            $('.colParticipantes').addClass('d-none');
-            $('.colInsumos').removeClass('d-none');
-        });
-        $('#collectionInsumos').on('click', function () {
-            $('.colInsumos').addClass('d-none');
-            $('.colServicos').removeClass('d-none');
-        });
-        $('#collectionServicos').on('click', function () {
-            $('.colServicos').addClass('d-none');
-            $('.colParticipantes').removeClass('d-none');
-        });
-    }
-
-}
-
-executeOnScreenResize();
-
+// * Responsivididade, basicamente altera entre uma tela e outra
+$('#collectionParticipantes').on('click', function () {
+    if (window.matchMedia("(min-width: 768px)").matches) return;
+    $('.colParticipantes').addClass('d-none');
+    $('.colInsumos').removeClass('d-none');
+});
+$('#collectionInsumos').on('click', function () {
+    if (window.matchMedia("(min-width: 768px)").matches) return;
+    $('.colInsumos').addClass('d-none');
+    $('.colServicos').removeClass('d-none');
+});
+$('#collectionServicos').on('click', function () {
+    if (window.matchMedia("(min-width: 768px)").matches) return;
+    $('.colServicos').addClass('d-none');
+    $('.colParticipantes').removeClass('d-none');
+});
 $(window).on('resize', function () {
-    executeOnScreenResize();
+    if ($(window).width() >= 768) {
+        $('.colInsumos, .colServicos, .colParticipantes').removeClass('d-none');
+    }
 });
 
+// * Blur nos outros accordions quando o botão em algum estiver em hover 
 $('#collectionInsumos').hover(
-    () => { $('.accordionParticipantes').toggleClass('blur'); $('.accordionServicos').toggleClass('blur') }
+    () => { $('.accordionParticipantes, .accordionServicos').toggleClass('blur') }
 )
 $('#collectionParticipantes').hover(
-    () => { $('.accordionInsumos').toggleClass('blur'); $('.accordionServicos').toggleClass('blur') }
+    () => { $('.accordionInsumos, .accordionServicos').toggleClass('blur') }
 )
 $('#collectionServicos').hover(
-    () => { $('.accordionInsumos').toggleClass('blur'); $('.accordionParticipantes').toggleClass('blur') }
+    () => { $('.accordionInsumos, .accordionParticipantes').toggleClass('blur') }
 )
-
 // > Se tiver 2 true o outro automaticamente será none
 var expandidos = [false, false, false]
 var elementos = [$('.colParticipantes'), $('.colInsumos'), $('.colServicos'),]
-
 $('#collectionParticipantes').on('click', () => {
 
-    elementos[1].toggleClass('col-md-4');
-    elementos[1].toggleClass('col-md-3');
-    elementos[2].toggleClass('col-md-4');
-    elementos[2].toggleClass('col-md-3');
-    elementos[0].toggleClass('col-md-6');
-    elementos[0].toggleClass('col-md-4');
+    $(elementos[1]).toggleClass('col-md-4 col-md-3');
+    $(elementos[2]).toggleClass('col-md-4 col-md-3');
+    $(elementos[0]).toggleClass('col-md-6 col-md-4');
 
     if (expandidos[0] === true) {
         expandidos[0] = false;
@@ -57,20 +47,14 @@ $('#collectionParticipantes').on('click', () => {
     } else {
         expandidos[0] = true;
         console.log("Partipantes minimizado");
-        if (expandidos[1] === true) {
-            elementos[2].addClass('d-none');
-        } else if (expandidos[2] === true) {
-            elementos[1].addClass('d-none');
-        }
+        elementos[2].toggleClass('d-none', expandidos[1] === true);
+        elementos[1].toggleClass('d-none', expandidos[2] === true);
     }
 });
 $('#collectionInsumos').on('click', () => {
-    elementos[0].toggleClass('col-md-4');
-    elementos[0].toggleClass('col-md-3');
-    elementos[2].toggleClass('col-md-4');
-    elementos[2].toggleClass('col-md-3');
-    elementos[1].toggleClass('col-md-4');
-    elementos[1].toggleClass('col-md-6');
+    $(elementos[0]).toggleClass('col-md-4 col-md-3');
+    $(elementos[2]).toggleClass('col-md-4 col-md-3');
+    $(elementos[1]).toggleClass('col-md-6 col-md-4');
 
     if (expandidos[1] === true) {
         expandidos[1] = false;
@@ -80,21 +64,15 @@ $('#collectionInsumos').on('click', () => {
     } else {
         expandidos[1] = true;
         console.log("Insumos expandido");
-        if (expandidos[0] === true) {
-            elementos[2].addClass('d-none');
-        } else if (expandidos[2] === true) {
-            elementos[0].addClass('d-none');
-        }
+        elementos[2].toggleClass('d-none', expandidos[0] === true);
+        elementos[0].toggleClass('d-none', expandidos[2] === true);
     }
 });
 $('#collectionServicos').on('click', () => {
 
-    elementos[0].toggleClass('col-md-4');
-    elementos[0].toggleClass('col-md-3');
-    elementos[1].toggleClass('col-md-4');
-    elementos[1].toggleClass('col-md-3');
-    elementos[2].toggleClass('col-md-4');
-    elementos[2].toggleClass('col-md-6');
+    $(elementos[0]).toggleClass('col-md-4 col-md-3');
+    $(elementos[1]).toggleClass('col-md-4 col-md-3');
+    $(elementos[2]).toggleClass('col-md-6 col-md-4');
 
     if (expandidos[2] === true) {
         expandidos[2] = false;
@@ -104,10 +82,39 @@ $('#collectionServicos').on('click', () => {
     } else {
         expandidos[2] = true;
         console.log("Insumos expandido");
-        if (expandidos[0] === true) {
-            elementos[1].addClass('d-none');
-        } else if (expandidos[1] === true) {
-            elementos[0].addClass('d-none');
-        }
+        elementos[1].toggleClass('d-none', expandidos[0] === true);
+        elementos[0].toggleClass('d-none', expandidos[1] === true);
     }
+});
+
+// * Ativar sombra de um accordion somente quando último item estiver colapsado
+var participantesExpandido = true;
+$('.accordionParticipantes .accordion-item:last-child .accordion-button').on('click', () => {
+    if (participantesExpandido === true) {
+        $('.accordionParticipantes').addClass('shadow');
+    } else {
+        $('.accordionParticipantes').removeClass('shadow');
+    }
+    participantesExpandido = !participantesExpandido;
+    console.log("Clicou");
+});
+var insumosExpandido = true;
+$('.accordionInsumos .accordion-item:last-child .accordion-button').on('click', () => {
+    if (insumosExpandido === true) {
+        $('.accordionInsumos').addClass('shadow');
+    } else {
+        $('.accordionInsumos').removeClass('shadow');
+    }
+    insumosExpandido = !insumosExpandido;
+    console.log("Clicou");
+});
+var servicosExpandido = true;
+$('.accordionServicos .accordion-item:last-child .accordion-button').on('click', () => {
+    if (servicosExpandido === true) {
+        $('.accordionServicos').addClass('shadow');
+    } else {
+        $('.accordionServicos').removeClass('shadow');
+    }
+    servicosExpandido = !servicosExpandido;
+    console.log("Clicou");
 });
