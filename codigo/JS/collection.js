c404db00ad3db5bbfc27b0bd3dd82f3aed37c721
Luis Brescia <luisfelipebrescia@gmail.com>
2023-05-26 10:59:29 -0300

    Animações introduzidas e ínicio da organização do arquivo CSS

--- codigo/JS/collection.js
+++ codigo/JS/collection.js
@@ -1,22 +1,33 @@
+var telaMD = false; // Para tela md ou maiores essa variável será true
+
 // * Responsivididade, basicamente altera entre uma tela e outra
 $('#collectionParticipantes').on('click', function () {
     if (window.matchMedia("(min-width: 768px)").matches) return;
+    console.log("Participantes");
     $('.colParticipantes').addClass('d-none');
     $('.colInsumos').removeClass('d-none');
 });
 $('#collectionInsumos').on('click', function () {
     if (window.matchMedia("(min-width: 768px)").matches) return;
+    console.log("Insumos");
     $('.colInsumos').addClass('d-none');
     $('.colServicos').removeClass('d-none');
 });
 $('#collectionServicos').on('click', function () {
     if (window.matchMedia("(min-width: 768px)").matches) return;
+    console.log("Servicos");
     $('.colServicos').addClass('d-none');
     $('.colParticipantes').removeClass('d-none');
 });
 $(window).on('resize', function () {
     if ($(window).width() >= 768) {
+        console.log("Tela MD");
         $('.colInsumos, .colServicos, .colParticipantes').removeClass('d-none');
+        telaMD = true;
+    } else if (telaMD === true) {
+        console.log("Tela SM");
+        $('.colInsumos, .colServicos').addClass('d-none');
+        telaMD = false;
     }
 });
 
@@ -25,7 +36,7 @@ $('#collectionInsumos').hover(
     () => { $('.accordionParticipantes, .accordionServicos').toggleClass('blur') }
 )
 $('#collectionParticipantes').hover(
-    () => { $('.accordionInsumos, .accordionServicos').toggleClass('blur') }
+    () => { $('.accordionInsumos, .accordionServicos').toggleClass('blur')}
 )
 $('#collectionServicos').hover(
     () => { $('.accordionInsumos, .accordionParticipantes').toggleClass('blur') }
@@ -34,7 +45,7 @@ $('#collectionServicos').hover(
 var expandidos = [false, false, false]
 var elementos = [$('.colParticipantes'), $('.colInsumos'), $('.colServicos'),]
 $('#collectionParticipantes').on('click', () => {
-
+    if (window.matchMedia("(max-width: 767px)").matches) return;
     $(elementos[1]).toggleClass('col-md-4 col-md-3');
     $(elementos[2]).toggleClass('col-md-4 col-md-3');
     $(elementos[0]).toggleClass('col-md-6 col-md-4');
@@ -52,6 +63,7 @@ $('#collectionParticipantes').on('click', () => {
     }
 });
 $('#collectionInsumos').on('click', () => {
+    if (window.matchMedia("(max-width: 767px)").matches) return;
     $(elementos[0]).toggleClass('col-md-4 col-md-3');
     $(elementos[2]).toggleClass('col-md-4 col-md-3');
     $(elementos[1]).toggleClass('col-md-6 col-md-4');
@@ -69,7 +81,7 @@ $('#collectionInsumos').on('click', () => {
     }
 });
 $('#collectionServicos').on('click', () => {
-
+    if (window.matchMedia("(max-width: 767px)").matches) return;
     $(elementos[0]).toggleClass('col-md-4 col-md-3');
     $(elementos[1]).toggleClass('col-md-4 col-md-3');
     $(elementos[2]).toggleClass('col-md-6 col-md-4');
