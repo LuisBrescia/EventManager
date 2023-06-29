4cbefa556cddd2213b4847dd4e0b8603f0e32a47
Luis Brescia <luisfelipebrescia@gmail.com>
2023-06-29 15:19:58 -0300

    webpack adicionado

--- codigo/webpack.config.js
+++ codigo/webpack.config.js
@@ -1,9 +1,26 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './HTML/Listas.html',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'html'),
+    open: true,
+    port: 3300,
   },
 };
