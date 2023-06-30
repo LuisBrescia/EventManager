287ac7d51bcfc8763f2954061212bcb01137a607
Luis Brescia <luisfelipebrescia@gmail.com>
2023-06-30 16:49:37 -0300

    Configurações de deploy

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,9 @@ export default defineConfig({
             input: {
                 main: resolve(__dirname, "./index.html"),
                 listas: resolve(__dirname, "./HTML/Listas.html"),
-                fluco: resolve(__dirname, "./HTML/Fluxo.html"),
+                fluxo: resolve(__dirname, "./HTML/Fluxo.html"),
             },
         },
     },
+    base: "/EventManager",
 });
