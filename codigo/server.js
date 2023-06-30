6b108d81ec3cccc8600e4d4e3ee763c39422dfde
Luis Brescia <luisfelipebrescia@gmail.com>
2023-06-30 17:45:08 -0300

    Servidor node onfigurado novamente

--- codigo/server.js
+++ codigo/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'public')));
+const app = express();
+const router = express.Router();
+
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // Defina os diretórios para as pastas de recursos estáticos
 app.use('/css', express.static(path.join(__dirname, 'css')));
@@ -11,24 +13,26 @@ app.use('/img', express.static(path.join(__dirname, 'img')));
 app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
 
 // Defina o diretório para os arquivos HTML
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'HTML', 'Listas.html'));
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'HTML/Listas.html'));
 });
-app.get('/Listas.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'HTML', 'Listas.html'));
+router.get('/Listas.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'HTML/Listas.html'));
 });
-app.get('/Fluxo.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'HTML', 'Fluxo.html'));
+router.get('/Fluxo.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'HTML/Fluxo.html'));
 });
-app.get('/Chamados.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'HTML', 'Chamados.html'));
+router.get('/Chamados.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'HTML/Chamados.html'));
 });
-app.get('/Collection.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'HTML', 'Collection.html'));
+router.get('/Collection.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'HTML/Collection.html'));
 });
-app.get('/Valores.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'HTML', 'Valores.html'));
+router.get('/Valores.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'HTML/Valores.html'));
 });
+
+app.use(router);
 
 // Defina a porta em que o servidor irá escutar
 const port = process.env.PORT || 3000;
